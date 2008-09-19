c3d90644c82d53d256ba6412ab7ecf2afe8c8d90
sjakub <sjakub@078c4a47-5f29-0410-8ae7-ff329e246042>
2008-09-19 18:05:28 +0000

    * Updated copyright year ;)

--- asus_oled/trunk/asus_oled.c
+++ asus_oled/trunk/asus_oled.c
@@ -1,7 +1,7 @@
 /*
  *  Asus OLED USB driver
  *
- *  Copyright (C) 2007 Jakub Schmidtke (sjakub@users.berlios.de)
+ *  Copyright (C) 2007,2008 Jakub Schmidtke (sjakub@users.berlios.de)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
